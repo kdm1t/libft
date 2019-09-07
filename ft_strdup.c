a1793b31aa64a5a929b31c21f7be0c0e637fdcb6
Mild Wilbur <mwilbur@oa-c4.21-school.ru>
2019-09-07 18:21:09 +0300

    07.09.2019

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwilbur <mwilbur@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/07 17:21:20 by mwilbur           #+#    #+#             */
+/*   Updated: 2019/09/07 18:16:56 by mwilbur          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*result;
+
+	i = 0;
+	result = NULL;
+	if (s1 != NULL)
+		result = (char*)malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (result == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwilbur <mwilbur@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/06 18:26:39 by mwilbur           #+#    #+#             */
+/*   Updated: 2019/09/07 18:18:58 by mwilbur          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *restrict dst, void *restrict *src, size_t n);
+char	*ft_strdup(const char *s1);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+size_t	ft_strlen(const char *s);
+
+#endif
