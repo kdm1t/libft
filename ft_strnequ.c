df8a0cb2182e8575f4d0cce49f16fab08a9d5425
Mild Wilbur <mwilbur@mi-w5.21-school.ru>
2019-09-20 12:29:39 +0300

    libft: done

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: mwilbur <mwilbur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/13 18:21:52 by mwilbur           #+#    #+#             */
-/*   Updated: 2019/09/13 18:33:19 by mwilbur          ###   ########.fr       */
+/*   Updated: 2019/09/20 12:23:10 by mwilbur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,9 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*first;
-	unsigned char	*second;
-
-	i = 0;
-	first = (unsigned char*)s1;
-	second = (unsigned char*)s2;
-	if (n == 1)
+	if (!s1 || !s2)
 		return (0);
-	while (i < n)
-	{
-		if (first[i] != second[i])
-			return (0);
-		i++;
-	}
-	return (1);
+	if (ft_strncmp(s1, s2, n) == 0)
+		return (1);
+	return (0);
 }
